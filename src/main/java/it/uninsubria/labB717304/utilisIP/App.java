a95b387a178d493de6a9540fe5a05dc66aa74ba5
GiuliaPais <giuliapais1@gmail.com>
2020-06-12 13:40:59 +0200

    Initial utilsIP commit

---
+++ src/main/java/it/uninsubria/labB717304/utilisIP/App.java
@@ -0,0 +1,13 @@
+package it.uninsubria.labB717304.utilisIP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
